2eb2ef3fcba4126d119695d19537748fd070d87c
anton <adeviatkin@intech-global.com>
2018-05-11 11:35:53 +0300

    1) Added class Parameter 2) Added handler for Enter for Textbox with data 3) Added handler for Click left button for textBox 4) ReInit DataGrid

--- ParsingPacket_wpf/MainWindow.xaml.cs
+++ ParsingPacket_wpf/MainWindow.xaml.cs
@@ -24,5 +24,34 @@ namespace ParsingPacket_wpf
         {
             InitializeComponent();
         }
+
+        public void ParsePacket(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Enter) {
+                Parameter param = new Parameter { Param = "Param", Value = "Value" };
+                
+                List<Parameter> list = new List<Parameter>();
+                list.Add(param);
+
+                param = new Parameter { Param="Hello", Value="World" };
+                list.Add(param);
+
+                param = new Parameter();
+
+                param.Param = "Hi";
+                param.Value = "WPF";
+                list.Add(param);
+
+                foreach (Parameter s in list) {
+                    outData.Items.Add(s);
+                }
+            }
+        }
+        public void ClickData(object sender, MouseButtonEventArgs e) {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                if (Data.Text == "Data")
+                    Data.Text = "";
+            }
+        }
     }
 }
